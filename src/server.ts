c91eaa6eaf0094f12e6c57c592b3c73d143bff9a
luc4sGabriel <lucas.g4briel.04@gmail.com>
2024-09-19 17:42:44 -0300

    db: Initial Config W Prisma

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
-import { env } from "./env";
-import { app } from "./app"
+import { env } from '@/env'
+import { app } from './app'
 
-app.listen({
+app
+  .listen({
     host: '0.0.0.0',
-    port: env.PORT
-}).then(() => {
-    console.log("Server running 🚀");
-});
-
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('Server running 🚀')
+  })
