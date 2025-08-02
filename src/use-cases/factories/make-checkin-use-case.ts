724fa15766af3b8d59c6378fa11050ea8da428ce
luc4sGabriel <lucas.g4briel.04@gmail.com>
2025-08-02 00:55:23 -0300

    feat: create factories for the uses cases

---
+++ src/use-cases/factories/make-checkin-use-case.ts
@@ -0,0 +1,11 @@
+import { CheckinUseCase } from "../check-in"
+import { PrismaCheckInsRepository } from "@/repositories/prisma/prisma-check-ins-repository"
+import { PrismaGymsRepository } from "@/repositories/prisma/prisma-gyms-repository"
+
+export function makeCheckInUseCase() {
+    const checkInsRepository = new PrismaCheckInsRepository()
+    const gymsRepository = new PrismaGymsRepository()
+    const useCase = new CheckinUseCase(checkInsRepository, gymsRepository)
+
+    return useCase
+}
