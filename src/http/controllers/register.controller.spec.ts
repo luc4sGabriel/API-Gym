5d6f01b68046d1c9d3bad754f3175303b800e491
luc4sGabriel <lucas.g4briel.04@gmail.com>
2025-08-22 13:53:32 -0300

    test: register test e2e

--- src/http/controllers/register.controller.spec.ts
+++ src/http/controllers/register.controller.spec.ts
@@ -1,5 +1,26 @@
-import { expect, test } from "vitest";
+import request from "supertest";
+import { app } from "../../app";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
 
-test('testing', () => {
-    expect(2 + 2).toEqual(4);
+describe('Register Controller', () => {
+    beforeAll(async () => {
+        await app.ready();
+    })
+
+    afterAll(async () => {
+        await app.close();
+    })
+
+
+    it('should be able to register', async () => {
+        const response = await request(app.server)
+            .post('/users')
+            .send({
+                name: 'John Doe 2',
+                email: 'johndoe@example.com',
+                password: '123456'
+            })
+
+        expect(response.statusCode).toEqual(201);
+    })
 })
